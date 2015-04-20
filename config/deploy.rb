d88c2f6ec9e29f93645a81acb91e3dc86b5dc958
Stuart Marquis <marquis@columbia.edu>
2015-04-20 09:49:04 -0400

    setup new cap deployment for clio_labs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :default_stage, 'clio_dev'
 # Temporarily define "morrison", for testing Unix Systems new nginx server
 # and "clio_eds", a config for demo-ing EDS integration only
-set :stages, %w(clio_dev clio_test clio_prod morrison clio_eds)
+set :stages, %w(clio_dev clio_test clio_prod morrison clio_eds clio_labs)
 
 require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
---
+++ config/deploy/clio_labs.rb
@@ -0,0 +1,14 @@
+
+set :rails_env, 'clio_labs'
+set :application, 'clio_labs'
+# n.b., hostname has no dash or underscore.
+set :domain,      'cliolabs.cul.columbia.edu'
+set :deploy_to,   "/opt/passenger/clio_labs/"
+set :user, 'deployer'
+set :scm_passphrase, 'Current user can full owner domains.'
+
+role :app, domain
+role :web, domain
+role :db,  domain, primary: true
+
+
