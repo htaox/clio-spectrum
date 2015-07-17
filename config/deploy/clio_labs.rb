3f9fb33469a090d9171bb1481bd642a61f296a9e
Stuart Marquis <marquis@columbia.edu>
2015-07-17 13:55:16 -0400

    Rebrand "Labs" to "Workshop" - only user-facing elements

--- config/deploy/clio_labs.rb
+++ config/deploy/clio_labs.rb
@@ -3,7 +3,7 @@ set :rails_env, 'clio_labs'
 set :application, 'clio_labs'
 # n.b., hostname has no dash or underscore.
 set :domain,      'cliolabs.cul.columbia.edu'
-set :deploy_to,   "/opt/passenger/clio_labs/"
+set :deploy_to,   "/opt/passenger/clio_workshop/"
 set :user, 'deployer'
 set :scm_passphrase, 'Current user can full owner domains.'
 
