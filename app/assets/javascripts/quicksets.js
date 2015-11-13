a0676ccea2b598def1663f49335b40d8fc95c04a
Stuart Marquis <marquis@columbia.edu>
2015-11-13 16:33:21 -0500

    quicksets - fix filter

--- app/assets/javascripts/quicksets.js
+++ app/assets/javascripts/quicksets.js
@@ -1,11 +1,15 @@
 $('#quick_set_content_provider_ids').selectMultiple({
 
-  selectableHeader: "<input type='text' class='qs1 search-input' autocomplete='off' placeholder='filter content provider list' size='40'>&nbsp;&nbsp;<span id='selected_count'>NNN content providers selected</span",
+  XselectableHeader: "<span>asdfasdf</span><input type='text' class='qs1 search-input' autocomplete='off' placeholder='filter content provider list' size='40'>",
+  selectableHeader: "<input type='text' class='qs1 search-input' autocomplete='off' placeholder='filter content provider list' size='40'>&nbsp;&nbsp;&nbsp;&nbsp;<span id='selected_count'></span",
 
   afterInit: function(ms){
     var that = this,
-        $selectableSearch = that.$selectableUl.prev(),
+        // $selectableSearch = that.$selectableUl.prev(),
+        $selectableSearch = $('.qs1'),
         selectableSearchString = '#'+that.$container.attr('id')+' .ms-elem-selectable';
+
+    $('#selected_count').text($('.ms-selected').length + " selected")
 
     that.qs1 = $selectableSearch.quicksearch(selectableSearchString)
     .on('keydown', function(e){
@@ -14,7 +18,6 @@ $('#quick_set_content_provider_ids').selectMultiple({
         return false;
       }
     });
-    $('#selected_count').text($('.ms-selected').length + " selected")
   },
 
   afterSelect: function(){
