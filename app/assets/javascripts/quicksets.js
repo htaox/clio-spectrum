9ec5d311d48a4a0d5f0c2005da45c73b18e0898c
Stuart Marquis <marquis@columbia.edu>
2015-11-13 16:18:15 -0500

    QuickSets - show Content Provider counts on edit form

--- app/assets/javascripts/quicksets.js
+++ app/assets/javascripts/quicksets.js
@@ -1,5 +1,7 @@
 $('#quick_set_content_provider_ids').selectMultiple({
-  selectableHeader: "<input type='text' class='qs1 search-input' autocomplete='off' placeholder='filter content provider list' size='40'>",
+
+  selectableHeader: "<input type='text' class='qs1 search-input' autocomplete='off' placeholder='filter content provider list' size='40'>&nbsp;&nbsp;<span id='selected_count'>NNN content providers selected</span",
+
   afterInit: function(ms){
     var that = this,
         $selectableSearch = that.$selectableUl.prev(),
@@ -12,11 +14,17 @@ $('#quick_set_content_provider_ids').selectMultiple({
         return false;
       }
     });
+    $('#selected_count').text($('.ms-selected').length + " selected")
   },
+
   afterSelect: function(){
     this.qs1.cache();
+    $('#selected_count').text($('.ms-selected').length + " selected")
   },
+
   afterDeselect: function(){
     this.qs1.cache();
+    $('#selected_count').text($('.ms-selected').length + " selected")
   }
+
 });
