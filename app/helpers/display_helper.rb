2caeeb2eef036a8c32cd2575cd94ab91e19b2bb6
Stuart Marquis <stuartmarquis@gmail.com>
2019-03-14 14:50:24 -0400

    Debug support

--- app/helpers/display_helper.rb
+++ app/helpers/display_helper.rb
@@ -532,12 +532,13 @@ module DisplayHelper
 
     value_txt = values.join("\n")
 
-    # if options[:expand_to] && !options[:expand_to].strip.empty?
-    #   value_txt += content_tag(:div, link_to(' more &#x25BC;'.html_safe, '#'),
-    #                            class: 'entry expander')
-    #   value_txt += content_tag(:div, options[:expand_to].html_safe,
-    #                            class: 'expander_more')
-    # end
+# DEBUG ONLY
+    if options[:expand_to] && !options[:expand_to].strip.empty?
+      value_txt += content_tag(:div, link_to('more', '#'),
+                               class: 'entry expander')
+      value_txt += content_tag(:div, options[:expand_to].html_safe,
+                               class: 'expander_more')
+    end
 
     value_txt = value_txt.html_safe
     value_txt
